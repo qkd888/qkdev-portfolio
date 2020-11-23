3e2c996fbd4eac04e8b9f54ebb148d5214e8ccd9
Quintin Kelly Dekker <quintinkellydekker888@gmail.com>
2020-11-23 13:20:42 +0200

    better

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,12 +4,12 @@ export default function About() {
     return (
         <div className="about">
             <h1>About</h1>
-            <p>Hello, My name is Quintin Kelly dekker</p>
+            <p>Hello, My name is Quintin Kelly Dekker</p>
             <p>
                 I am fueled by high energy levels and boundless enthusiasm, I’m easily
                 inspired and more then willing to follow my fascinations wherever they
-                take me. I’m passionate, expressive,and a multi-talented spirit with a
-                natural ability to entertain and inspire. I’m never satisfied to just
+                take me. I’m passionate, expressive, and a multi-talented spirit with
+                a natural ability to entertain and inspire. I’m never satisfied to just
                 come up with ideas. Instead I have an almost impulsive need to act on
                 them.
       </p>
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -3,9 +3,14 @@ import React from "react";
 
 
 
+
+
+
 export default function Landing() {
     return (
         <div>
+
+
             <h1 className="heading">&lt;  Quintin Kelly Dekker / &gt;{" "}</h1>
             <h3 className="subHeading">
                 &lt; Junior Full Stack Web Developer / &gt;{" "}
